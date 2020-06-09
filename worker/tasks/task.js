91c61bd4049f58ffcb68667eaf4c112e41a0cf99
Łukasz Zapolski <lukasz.zapolski@booksy.com>
2020-06-09 22:34:28 +0200

    Add Task class

---
+++ worker/tasks/task.js
@@ -0,0 +1,31 @@
+const fetch = require('node-fetch')
+const redis = require("redis")
+const client = redis.createClient()
+
+const { promisify } = require('util')
+const setAsync = promisify(client.set).bind(client)
+
+class Task {
+    constructor (taskName, baseUrl) {
+        this.taskName = taskName
+        this.baseUrl = baseUrl
+        this.jobs = []
+    }
+
+    fetchJobs = async () => {
+        let resultCount = 1
+        let onPage = 0
+
+        while(resultCount > 0) {
+            const res = await fetch(`${this.baseUrl}?page=${onPage}`)
+            const jobs = await res.json()
+            this.jobs = [...this.jobs, ...jobs]
+            resultCount = jobs.length
+            onPage++
+        }
+
+        await setAsync(this.taskName, JSON.stringify(this.jobs))
+    }
+}
+
+module.exports = Task
