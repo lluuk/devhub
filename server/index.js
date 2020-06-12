badc5c49dedb26cfab2877d71fef19b3fdbc8ae8
Łukasz Zapolski <lukasz.zapolski@booksy.com>
2020-06-12 15:26:46 +0200

    Add endpoint for jobs

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3000
+
+const redis = require("redis"), client = redis.createClient()
+
+const { promisify } = require('util')
+const getAsync = promisify(client.get).bind(client)
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/api/jobs', async (req, res) => {
+    //pagination, sorting
+    const githubJobs = await getAsync('github')
+    const soJobs = await getAsync('so')
+    const jobs = [...JSON.parse(githubJobs), ...JSON.parse(soJobs)]
+
+    return res.send(jobs)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
